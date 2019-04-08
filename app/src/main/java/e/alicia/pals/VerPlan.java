270f044b5ca89a556d13b39c74a124897fb7b501
alicia <aliciavisual@gmail.com>
2019-04-08 17:08:07 +0200

    recycler view funcionando

--- app/src/main/java/e/alicia/pals/VerPlan.java
+++ app/src/main/java/e/alicia/pals/VerPlan.java
@@ -2,6 +2,7 @@ package e.alicia.pals;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
 
 public class VerPlan extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class VerPlan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_plan);
+
     }
 }
